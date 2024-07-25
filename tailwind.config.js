f3c0201267c62091537e8c6881b5fb65899d68ae
sardor <136901570+salimovone@users.noreply.github.com>
2024-07-25 14:02:31 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkbg: "#1e0338"
+      }
+    },
+  },
+  plugins: [],
+}
