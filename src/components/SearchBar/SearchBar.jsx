1d27c9893db0c959c0f69c78c39ec3508ea0130f
sodiqdevpython <sodiqdevpython@gmail.com>
2024-07-26 16:49:56 +0500

    jcdsb

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci";
 export default function SearchBar() {
     return (
         <div className="flex w-96 h-10 rounded-full border border-[#dc66a2] bg-white">
-            <div className='flex justify-center items-center w-[20%]'>
+            <div className='flex cursor-pointer justify-center items-center w-[20%]'>
                 <CiSearch className='text-3xl text-[#858585]' />
             </div>
             <div className='flex w-[80%] justify-start items-center'>
