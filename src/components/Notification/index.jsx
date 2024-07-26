5b92e0b91daf5913b0921a0a66f9ae9e1be6a445
sodiqdevpython <sodiqdevpython@gmail.com>
2024-07-26 16:40:46 +0500

    ajvkfdbvnied

--- src/components/Notification/index.jsx
+++ src/components/Notification/index.jsx
@@ -4,8 +4,8 @@ import { IoIosNotificationsOutline } from "react-icons/io";
 
 export default function Notification({ number = 0 }) {
     return (
-        <div className='relative'>
-            <p className='bg-#db66a2 rounded-full'>{number}</p>
+        <div className='max-w-10 cursor-pointer font-poppins'>
+            <p className='bg-gradient-to-r from-purple-500 to-pink-500 rounded-full w-10 text-center text-Light'>{number}</p>
             <IoIosNotificationsOutline className='text-[#858585] text-4xl bg-white' />
         </div>
     )
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SearchBar from '../components/SearchBar/SearchBar'
 import ProfileView from '../components/ProfileView/ProfileView'
 import Notification from '../components/Notification'
+import NavbarItem from '../components/NavbarItems'
 import { profileImage } from '../assets'
 
 export default function Test() {
@@ -10,7 +11,14 @@ export default function Test() {
             <div className='flex flex-col gap-36 mt-10 ms-5'>
                 <SearchBar />
                 <ProfileView img={profileImage} fullName={'Ramon Ridwan'} />
-                <Notification />
+                <Notification number={123} />
+                <div className='w-80'>
+                    <NavbarItem title={'Boshqaruv qismi'} isActive={0} />
+                </div>
+                <div className='w-80'>
+                    <NavbarItem title={'Boshqaruv qismi'} isActive={1} />
+                </div>
+
             </div>
         </div>
     )
