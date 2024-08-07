4564c15738234bca6ad90af594b653f7ebaa5f35
sardor salimov <salimovone@gmail.com>
2024-08-07 03:17:27 -0500

    cards near complete

---
+++ src/components/OverviewCards/Card/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./card.module.css";
+
+const Card = ({ num, title }) => {
+  return (
+    <div className="bg-white rounded-2xl border p-8 flex justify-between gap-8 relative">
+      <div>
+        <div className={styles.spinner}>
+          <div className={styles.spinner1}>{num}</div>
+        </div>
+      </div>
+
+      <div className="flex justify-end items-center">
+        <span className="font-bold text-Secondary text-lg">{title}</span>
+      </div>
+      <span className="absolute bottom-5 right-8 text-violet-600">{num}</span>
+    </div> 
+  );
+};
+
+export default Card;
--- src/pages/MainDashboard/index.jsx
+++ src/pages/MainDashboard/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import OverviewCards from '../../components/OverviewCards'
+import Table from '../../components/Table'
 
 const MainDashboard = () => {
   return (
     <div className='w-full h-full bg-[#fff9fe] pt-5 px-10'>
+
+      <div className="w-full flex justify-between items-center text-Secondary text-sm">
+        <span className='font-bold'>User management system overview</span>
+        <span>Accounts | Ramon Ridwan</span>
+      </div>
+
+      <OverviewCards />
+      <Table />
+
     </div>
   )
 }
