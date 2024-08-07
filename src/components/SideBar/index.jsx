530eaa41de96c4775981988514467a41039f5ed2
sardor salimov <salimovone@gmail.com>
2024-08-07 02:22:46 -0500

    sidebar near complete

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import data from "./data";
+import NavbarItem from "../NavbarItem";
+import { useNavigate } from "react-router-dom";
 
 const SideBar = () => {
-  return (
-    <div className='w-full h-full'>SideBar</div>
-  )
-}
+  const [active, setActive] = useState(0);
+  const navigate = useNavigate()
 
-export default SideBar
+  const clickItem = (id, path) => {
+    setActive(id)
+    navigate(path)
+  }
+  return (
+    <div className="w-full h-full border-r pr-4 pt-10">
+      {data.map((itm) => (
+        <React.Fragment key={itm.id}>
+          <NavbarItem
+            icon={itm.icon}
+            title={itm.name}
+            isActive={active === itm.id}
+            onClick={() => clickItem(itm.id, itm.path)}
+          />
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default SideBar;
